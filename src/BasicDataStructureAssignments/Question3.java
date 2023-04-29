550fb9ff0534a203760937199d08d2c93d11ef2d
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-04-29 22:42:48 +0530

    commit

--- src/BasicDataStructureAssignments/Question3.java
+++ src/BasicDataStructureAssignments/Question3.java
@@ -1,5 +1,10 @@
 package BasicDataStructureAssignments;
+class SiCi {
+    public double simpleInterest(double principalAmount,int time,double interestRate){
 
+    }
+    public double compoundInterest(double principalAmount,int time,double interestRate){}
+}
 public class Question3 {
     public static void main(String[] args) {
 
