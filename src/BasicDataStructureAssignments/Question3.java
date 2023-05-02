0c5aba5ff66d740c6c0c3071f4187c68261f3554
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-05-02 21:53:06 +0530

    commit

--- src/BasicDataStructureAssignments/Question3.java
+++ src/BasicDataStructureAssignments/Question3.java
@@ -1,12 +1,28 @@
 package BasicDataStructureAssignments;
+
+import java.util.Scanner;
+
 class SiCi {
     public double simpleInterest(double principalAmount,int time,double interestRate){
-
+        double simpleinterest = (principalAmount*time*interestRate)/100;
+        return simpleinterest;
     }
-    public double compoundInterest(double principalAmount,int time,double interestRate){}
+    public double compoundInterest(double principalAmount,int time,double interestRate){
+        double compoundinterest = principalAmount*(Math.pow((1+interestRate),time))-principalAmount;
+        return compoundinterest;
+    }
 }
 public class Question3 {
     public static void main(String[] args) {
+        SiCi object = new SiCi();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Principal Amount");
+        double principalAmount = sc.nextDouble();
+        System.out.println("Enter time");
+        int time = sc.nextInt();
+        System.out.println("Enter interestRate");
+        double interestRate = sc.nextDouble();
+        double simpleinterest = object.simpleInterest(principalAmount,time,interestRate);
 
     }
 }
