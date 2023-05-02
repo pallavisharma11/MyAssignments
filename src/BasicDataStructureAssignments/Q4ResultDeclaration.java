e57b20a9291fa37cb0e4f400f153a0ff879069cf
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-05-02 22:12:09 +0530

    commit

--- src/BasicDataStructureAssignments/Q4ResultDeclaration.java
+++ src/BasicDataStructureAssignments/Q4ResultDeclaration.java
@@ -1,5 +1,9 @@
 package BasicDataStructureAssignments;
-
+class ResultDeclaration{
+    public String declareResults( double subject1Marks, double subject2Marks, double subject3Marks) {
+        return "pallavi";
+    }
+}
 public class Q4ResultDeclaration {
     public static void main(String[] args) {
 
