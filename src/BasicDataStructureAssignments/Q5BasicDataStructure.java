74b8db4142688906644e5361bce9fdf989d28627
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-05-04 22:30:16 +0530

    commit

--- src/BasicDataStructureAssignments/Q5BasicDataStructure.java
+++ src/BasicDataStructureAssignments/Q5BasicDataStructure.java
@@ -1,6 +1,19 @@
 package BasicDataStructureAssignments;
+
+import java.util.Scanner;
+
 class TaxAmount{
-    public double calculateTaxAmount(int ctc){}
+    public double calculateTaxAmount(int ctc){
+
+    }
 }
 public class Q5BasicDataStructure {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter CTC");
+        int CTC = sc.nextInt();
+        double taxAmount = new TaxAmount().calculateTaxAmount(CTC);
+        System.out.println("Calculate Tax Amount :- "+ +taxAmount);
+
+    }
 }
