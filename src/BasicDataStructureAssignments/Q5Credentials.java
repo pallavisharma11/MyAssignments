e84c0dd39c3611f41b66346dc9f830396d1beb31
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-05-06 22:40:09 +0530

    commit

--- src/BasicDataStructureAssignments/Q5Credentials.java
+++ src/BasicDataStructureAssignments/Q5Credentials.java
@@ -1,5 +1,6 @@
 package BasicDataStructureAssignments;
-
+class Login{
+    String userId = "Ajay",password = "password";
 public class Q5Credentials {
     public static void main(String[] args) {
 
