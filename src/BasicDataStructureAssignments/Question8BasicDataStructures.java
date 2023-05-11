75356914690d7ecb84a73fc370ecc62a41e49f0d
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-05-11 23:09:37 +0530

    commit

---
+++ src/BasicDataStructureAssignments/Question8BasicDataStructures.java
@@ -0,0 +1,39 @@
+package BasicDataStructureAssignments;
+class BubbleSort {
+
+    public int[] bubbleSort(int arr[]) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    //swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+        return arr;
+    }
+}
+public class Question8BasicDataStructures {
+    public static void main(String args[]) {
+        int arr[] = {5,12,14,6,78,19,1,23,26,35,37,7,52,86,47};
+        BubbleSort object = new BubbleSort();
+        System.out.println("Array Before Bubble Sort");
+        for(int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        object.bubbleSort(arr);//sorting array elements using bubble sort
+
+        System.out.println("Array After Bubble Sort");
+        for(int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
---
+++ src/BasicDataStructureAssignments/Question9.java
@@ -0,0 +1,70 @@
+package BasicDataStructureAssignments;
+
+import java.util.Scanner;
+
+class Student {
+    private int subjectA,subjectB,subjectC;
+    Student(int subjectA,int subjectB,int  subjectC) {
+        this.subjectA = subjectA;
+        this.subjectB = subjectB;
+        this.subjectC = subjectC;
+    }
+
+
+
+    public static  int studentsTotalMarksInAllSubjects(Student[] students) {
+        int sum =0;
+        for(int i =0;i<students.length;i++){
+            sum = sum+ students[i];
+        }
+    }
+
+    public double studentsAverageMarksInAllSubjects(Student[] students) {
+        double ava=studentsTotalMarksInAllSubjects(students)/9;
+        return ava;
+    }
+    public int[] subjectWiseMarks(Student[] students,String subjectName){
+
+    }
+    public int subjectATotalByStudents(int[] marks) {
+
+    }
+    public int subjectBTotalByStudents(int[] marks) {}
+    public int subjectCTotalByStudents(int[] marks) {}
+
+    public int subjectTotalByStudents(int[] marks)
+
+    public double subjectAAverageByStudents(int[] marks) {}
+    public double subjectBAverageByStudents(int[] marks) {}
+    public double subjectCAverageByStudents(int[] marks) {}
+
+}
+public class Question9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student[] students = new Student[9];
+        System.out.println("marks of Student 1 in subjects A,B,C");
+        //int[] s1 = new int[3];
+        int s1A = sc.nextInt();
+        int s1B= sc.nextInt();
+        int s1C = sc.nextInt();
+        students[1] = new Student(s1A,s1B,s1C);
+        System.out.println("marks of Student 2 in subjects A,B,C");
+        int s2A = sc.nextInt();
+        int s2B= sc.nextInt();
+        int s2C = sc.nextInt();
+        students[2] = new Student(s2A,s2B,s2C);
+        System.out.println("marks of Student 3 in subjects A,B,C");
+        int s3A = sc.nextInt();
+        int s3B= sc.nextInt();
+        int s3C = sc.nextInt();
+        students[3] = new Student(s3A,s3B,s3C);
+        int[] marks =
+
+
+
+
+
+
+    }
+}
