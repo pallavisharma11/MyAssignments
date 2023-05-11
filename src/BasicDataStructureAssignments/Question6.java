66148e0cb15bbfe68e7046c5a47ec0022026b33d
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-05-11 22:14:41 +0530

    commit

--- src/BasicDataStructureAssignments/Question6.java
+++ src/BasicDataStructureAssignments/Question6.java
@@ -1,10 +1,36 @@
 package BasicDataStructureAssignments;
 
+import java.util.Scanner;
+
+class Login{
+    String userId = "Ajay",password = "password";
+    public String loginUser(String user, String pass) {
+        if(user.equals(userId) && pass.equals(password)){
+            return "Welcome "+user;
+        }
+        else if (user.equals(userId) && pass.equals(password)){
+            return "You have entered wrong credentials ,please enter the right credentials.";
+        }
+        else{
+            return "You have entered wrong credentials 3 times";
+        }
+    }
+
+}
 public class Question6
 {
     public static void main(String[] args) {
-        System.out.println(" ");
-        System.out.println("  ");
+        Scanner sc = new Scanner(System.in);
+        Login object = new Login();
+        int count =3;
+        while(count!=3){
+            System.out.println("Enter UserId");
+            String userId = sc.next();
+            System.out.println("Enter Password");
+            String password = sc.next();
+            System.out.println(object.loginUser(userId,password));
+        }
+
 
     }
 }
