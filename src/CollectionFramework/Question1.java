19504ed30a823c44a643ec1497657d9f1cc66ab4
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-05-16 21:58:17 +0530

    commit

---
+++ src/CollectionFramework/Question1.java
@@ -0,0 +1,22 @@
+package CollectionFramework;
+import java.util.*;
+class Contact{
+    Long phoneNumber;
+    String name;
+    String email;
+    String gender;
+    Contact(String name,String email,String gender){
+//        this.phoneNumber=phoneNumber;
+        this.name=name;
+        this.email=email;
+        this.gender=gender;
+    }
+}
+public class Question1 {
+    TreeMap<Long,Contact> map=new TreeMap<Long,Contact>();
+    Contact c1 = new Contact("Pallavi","pallavi@gmail.com","Female");
+    Contact c2 = new Contact("Parth","parth@gmail.com","Male");
+    Contact c3 = new Contact("Kashish","kashish@gmail.com","Female");
+    map.put(123456789,c1);
+
+}
