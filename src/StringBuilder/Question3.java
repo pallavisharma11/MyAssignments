bf1ec91d0acbd5dc17f4df30d33ff2183717455c
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-05-18 22:00:06 +0530

    commit

--- src/StringBuilder/Question3.java
+++ src/StringBuilder/Question3.java
@@ -1,11 +1,13 @@
 package StringBuilder;
 
 public class Question3 {
+    Question3 obj = new Question3();
     public static void main(String args[])
     {
         StringBuffer sbf = new StringBuffer("This methods returns the reversed the object on which it is called");
         sbf.reverse();
         System.out.println("String buffer after reversing = " + sbf);
+        
 
     }
 }
