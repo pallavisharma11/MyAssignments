3d64a98de47bdb9ef892618037863d06607dc4d3
pallavisharma11 <pallavi.sharma_cs19@gla.ac.in>
2023-05-19 11:02:47 +0530

    commit

--- src/CollectionFramework/Question1.java
+++ src/CollectionFramework/Question1.java
@@ -5,18 +5,38 @@ class Contact{
     String name;
     String email;
     String gender;
-    Contact(String name,String email,String gender){
-//        this.phoneNumber=phoneNumber;
+    Contact(Long phoneNumber,String name,String email,String gender){
+        this.phoneNumber= phoneNumber;
         this.name=name;
         this.email=email;
         this.gender=gender;
     }
+    public Long getPhoneNumber(){
+        return phoneNumber;
+    }
+    public String toString(){
+        return name+" "+email+" "+gender;
+    }
+}
+class SortAccordingPhoneNumber implements Comparator<Contact>{
+    @Override
+    public int compare(Contact c1, Contact c2) {
+        return c2.getPhoneNumber().compareTo(c1.getPhoneNumber());
+    }
 }
 public class Question1 {
-    TreeMap<Long,Contact> map=new TreeMap<Long,Contact>();
-    Contact c1 = new Contact("Pallavi","pallavi@gmail.com","Female");
-    Contact c2 = new Contact("Parth","parth@gmail.com","Male");
-    Contact c3 = new Contact("Kashish","kashish@gmail.com","Female");
+    public static void main(String[] args) {
+
+        TreeMap <Contact,Long> map = new TreeMap<>(new SortAccordingPhoneNumber());
+        map.put(new Contact(1234567891l ,"Pallavi", "pallavi@gmail.com", "Female"),1234567891l);
+        map.put(new Contact(8889993331l,"Parth","parth@gmail.com","Male"),8889993331l);
+        map.put(new Contact(9959967006l,"Kashish","kashish@gmail.com","Female"),9959967006l);
+        for (Map.Entry<Contact, Long> entry :
+                map.entrySet()) {
+            System.out.println(entry.getKey() +" "+ entry.getValue());
+        }
+
+    }
 
 
 }
--- src/CollectionFramework/Question3.java
+++ src/CollectionFramework/Question3.java
@@ -1,92 +1,117 @@
-package CollectionFramework;
-
-import java.util.Comparator;
-import  java.util.TreeSet;
-import java.util.Scanner;
-class Employee{
-    private int id;
-    private String name;
-    private String department;
-    private double salary;
-
-    public Employee(int id, String name,String department,double salary){
-        this.id = id;
-        this.name=name;
-        this.department=department;
-        this.salary = salary;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getDepartment() {
-        return department;
-    }
-    public void setDepartment(String name) {
-        this.department = department;
-    }
-    public double getSalary() {
-        return salary;
-    }
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-}
-public class Question3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        TreeSet<Employee> map = new TreeSet();
-        map.add(new Employee(2,"Pallavi","software",50000));
-        map.add(new Employee(5,"Kashish","Engineer",16000));
-        map.add(new Employee(1,"Ashish","Engineer",40000));
-        map.add(new Employee(9,"Priya","HR",100000));
-        map.add(new Employee(8,"Hemlata","Testing",55000));
-        map.add(new Employee(2,"Ankit","Testing",580000));
-        map.add(new Employee(3,"Ankita","software",5000));
-        map.add(new Employee(6,"Govind","HR",10000));
-        map.add(new Employee(10,"Laksh","software",67000));
-        map.add(new Employee(4,"Pranjali","HR",6700));
-        map.add(new Employee(2,"Parth","Testing",500000));
-        map.add(new Employee(7,"Prashat","Testing",700000));
-
-        System.out.println("ID");
-        System.out.println("Name");
-        System.out.println("Department");
-        System.out.println("Salary");
-        System.out.println("Your choice");
-        int choice = sc.nextInt();
-        switch (choice){
-            case 'a': class MyNameComp implements Comparator<Employee>{
-
-                    @Override
-                    public int compare(Employee e1, Employee e2) {
-                        return e1.getId().compareTo(e2.getId());
-                    }
-                    
-                }
-                break;
-            case 'b':class MyNameComp1 implements Comparator<Employee>{
-
-                @Override
-                public int compare(Employee e1, Employee e2) {
-                    return e1.getName().compareTo(e2.getName());
-                }
-
-            }
-                break;
-
-            case 'c':
-        }
-
-    }
-}
+//package CollectionFramework;
+//
+//import java.util.Comparator;
+//import  java.util.TreeSet;
+//import java.util.Scanner;
+//class Employee implements Comparable<Employee>{
+//    private int id;
+//    private String name;
+//    private String department;
+//    private double salary;
+//
+//    public Employee(int id, String name,String department,double salary){
+//        this.id = id;
+//        this.name=name;
+//        this.department=department;
+//        this.salary = salary;
+//        public int compareTo(Employee e)
+//        {
+//            return this.id - e.year;
+//        }
+//    }
+//    public int getId() {
+//        return id;
+//    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    public String getDepartment() {
+//        return department;
+//    }
+//    public void setDepartment(String name) {
+//        this.department = department;
+//    }
+//    public double getSalary() {
+//        return salary;
+//    }
+//    public void setSalary(int salary) {
+//        this.salary = salary;
+//    }
+//    abstract compareTo()
+//
+//}
+//public class Question3 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        TreeSet<Employee> map = new TreeSet();
+//        map.add(new Employee(2,"Pallavi","software",50000));
+//        map.add(new Employee(5,"Kashish","Engineer",16000));
+//        map.add(new Employee(1,"Ashish","Engineer",40000));
+//        map.add(new Employee(9,"Priya","HR",100000));
+//        map.add(new Employee(8,"Hemlata","Testing",55000));
+//        map.add(new Employee(2,"Ankit","Testing",580000));
+//        map.add(new Employee(3,"Ankita","software",5000));
+//        map.add(new Employee(6,"Govind","HR",10000));
+//        map.add(new Employee(10,"Laksh","software",67000));
+//        map.add(new Employee(4,"Pranjali","HR",6700));
+//        map.add(new Employee(2,"Parth","Testing",500000));
+//        map.add(new Employee(7,"Prashat","Testing",700000));
+//
+//        System.out.println("ID");
+//        System.out.println("Name");
+//        System.out.println("Department");
+//        System.out.println("Salary");
+//        System.out.println("Your choice");
+//        int choice = sc.nextInt();
+//        switch (choice){
+//            case 'a': class MyNameComp implements Comparator<Employee>{
+//
+//                    @Override
+//                    public int compare(Employee e1, Employee e2) {
+////                        return e1.getId().compareTo(e2.getId());
+//                        return 1;
+//                    }
+//
+//                }
+//                break;
+//            case 'b':class MyNameComp1 implements Comparator<Employee>{
+//
+//                @Override
+//                public int compare(Employee e1, Employee e2) {
+//                    return e1.getName().compareTo(e2.getName());
+//                }
+//
+//            }
+//                break;
+//
+//            case 'c':
+//                class MyNameComp2 implements Comparator<Employee>{
+//
+//                    @Override
+//                    public int compare(Employee e1, Employee e2) {
+//                        return e1.getName().compareTo(e2.getName());
+//                    }
+//
+//                }
+//                break;
+//            case 'd':
+//                class MyNameComp3 implements Comparator<Employee>{
+//
+//                    @Override
+//                    public int compare(Employee e1, Employee e2) {
+//                        return e1.getName().compareTo(e2.getName());
+//                    }
+//
+//                }
+//                break;
+//        }
+//
+//    }
+//}
